e08d51a5945ca5edbcd3b1f0cd0c9ff76428cf00
Gabby Choonoo <choonoo@ohsu.edu>
2017-05-04 10:56:33 -0700

    Update array_qa_qc_functions.r

--- array_qa_qc_functions.r
+++ array_qa_qc_functions.r
@@ -19,9 +19,9 @@ It is meant to provide help documentation in the same vein as Docstrings in Pyth
 
 make.colors.boxplot <- function(col.strains)
 {
-    use.cols <- darkColors(length(unique(col.strains)))
-    names(use.cols) <- unique(col.strains)
-    return(as.character(use.cols[col.strains]))
+  use.cols <- darkColors(length(unique(col.strains)))
+  names(use.cols) <- unique(col.strains)
+  return(as.character(use.cols[col.strains]))
 }
 
 make.boxplot <- function(use.exprs, type=c("raw", "norm"), make.pdf=F, base.name="test", order.by=c("Mating", "Sex", "Number"), color.by="Mating", highlight.names=NULL, ...)
@@ -49,7 +49,7 @@ make.boxplot <- function(use.exprs, type=c("raw", "norm"), make.pdf=F, base.name
     plot(x=1:ncol(use.exprs), y=pData(use.exprs)$RIN, xlim=c(1, ncol(use.exprs)) + c(-.5, .5), ylim=range(pData(use.exprs)$RIN, na.rm=T), xaxt="n", type="p", ylab="RIN", xlab="", col=make.colors.boxplot(pData(use.exprs)[,color.by]))
     abline(h=7, lty="dashed")
   } else {
-	warning("RIN values were not found in the sample annotations.")
+    warning("RIN values were not found in the sample annotations.")
   }
   
   #labels and such
@@ -77,8 +77,8 @@ make.boxplot <- function(use.exprs, type=c("raw", "norm"), make.pdf=F, base.name
   
   if (make.pdf) {
     d = dev.copy2pdf(file=paste0(base.name, '_', type, '_boxplot.pdf'), width=16, height=10)
-	writeLines(paste0("Figure saved to: ", paste0(base.name, '_', type, '_boxplot.pdf')))
-	d = dev.off()
+    writeLines(paste0("Figure saved to: ", paste0(base.name, '_', type, '_boxplot.pdf')))
+    d = dev.off()
   }
 }
 attr(make.boxplot, 'help') = "
@@ -91,10 +91,10 @@ type: The type of plot to create, either 'raw' or 'norm'.
 make.pdf: A logical indicating if a PDF should be created. 
 base.name: The base filename of the PDF to create (default is 'test') 
 order.by: A character vector containing the column names that will be used to order 
-   the samples (default is c('Mating', 'Sex', 'Number')).
+the samples (default is c('Mating', 'Sex', 'Number')).
 color.by: The column name used to assign colors to the samples (default is 'Mating'). 
 highlight.names: A character vector containing sample names (default=NULL). Can be used 
-   to highlight samples in the plot. 
+to highlight samples in the plot. 
 ...: Additional parameters can be passed to format the x-axis labels.
 "
 
@@ -109,38 +109,46 @@ highlight.names: A character vector containing sample names (default=NULL). Can 
 
 plot.bac.spikes <- function(use.exprs, pgf.file, base.name="test", make.pdf=F)
 {
-    require(ggplot2)
-    require(affxparser)
-    #use.exprs <- rma(raw.exprs)
-    
-	if (file.exists(pgf.file)) {
-		pgf.list <- readPgf(pgf.file)
-	} else {
-		stop("Probe group file not found!")
-	}
-    probeset.types <- data.frame(fsetid=pgf.list$probesetId, fsetName=pgf.list$probesetName, type=pgf.list$probesetType, stringsAsFactors=FALSE)
-    
-    spike.probes <- probeset.types[probeset.types$type == 'control->affx->bac_spike',]
-
-    spike.exprs <- exprs(use.exprs)[as.character(unique(spike.probes$fsetid)),]
-    
-    spike.dta <- melt(spike.exprs)
-    spike.dta.merge <- merge(spike.dta, spike.probes, by.x="Var1", by.y="fsetid", all=F, incomparables=NA, sort=FALSE)
-    
-    spike.dta.merge$`Bac. Spike` <- sapply(strsplit(spike.dta.merge$fsetName, "-"), "[[", 4)
-    #should be BioB<BioC<BioD<Cre
-    spike.dta.merge$`Bac. Spike` <- factor(spike.dta.merge$`Bac. Spike`, levels=c("cre", "bioD", "bioC", "bioB"), labels=c("Cre", "BioD", "BioC", "BioB"), ordered=TRUE)
-    
-    bac.plot <- qplot(x=Var2, y=value, group=`Bac. Spike`, color=`Bac. Spike`, data=spike.dta.merge, stat="summary", fun.y=mean, geom="line", ylab="log2(Expression)", xlab="", main="Bacterial Spikes")
-	bac.plot <- bac.plot + theme(axis.text.x=element_text(size=8, angle=90, hjust=1))
-    
-	plot(bac.plot)
-	
-    if (make.pdf) {
-        dev.copy2pdf(file=paste0(base.name, "_bac_spikes.pdf"), width=16, height=10)
-		writeLines(paste0("Figure saved to: ", paste0(base.name, "_bac_spikes.pdf")))
-		d = dev.off()
-    }
+  require(ggplot2)
+  require(affxparser)
+  #use.exprs <- rma(raw.exprs)
+  
+  if (file.exists(pgf.file)) {
+    pgf.list <- readPgf(pgf.file)
+  } else {
+    stop("Probe group file not found!")
+  }
+  probeset.types <- data.frame(fsetid=pgf.list$probesetId, fsetName=pgf.list$probesetName, type=pgf.list$probesetType, stringsAsFactors=FALSE)
+  
+  spike.probes <- probeset.types[probeset.types$type == 'control->affx->bac_spike',]
+  
+  spike.exprs <- exprs(use.exprs)[as.character(unique(spike.probes$fsetid)),]
+  
+  spike.dta <- melt(spike.exprs)
+  spike.dta.merge <- merge(spike.dta, spike.probes, by.x="Var1", by.y="fsetid", all=F, incomparables=NA, sort=FALSE)
+  
+  spike.dta.merge$`Bac. Spike` <- sapply(strsplit(spike.dta.merge$fsetName, "-"), "[[", 4)
+  #should be BioB<BioC<BioD<Cre
+  spike.dta.merge$`Bac. Spike` <- factor(spike.dta.merge$`Bac. Spike`, levels=c("cre", "bioD", "bioC", "bioB"), labels=c("Cre", "BioD", "BioC", "BioB"), ordered=TRUE)
+  
+  #bac.plot <- qplot(x=Var2, y=value, group=`Bac. Spike`, color=`Bac. Spike`, data=spike.dta.merge, geom="line", ylab="log2(Expression)", xlab="", main="Bacterial Spikes", method="summary")
+  #bac.plot <- bac.plot + theme(axis.text.x=element_text(size=8, angle=90, hjust=1))
+  
+  bac.plot <- ggplot(spike.dta.merge, aes(x=Var2, y=value, group=`Bac. Spike`, color=`Bac. Spike`)) +
+    stat_summary(fun.y=mean, geom="line") +
+    ylab("log2(Expression)") +
+    xlab("") +
+    ggtitle("Bacterial Spikes") + 
+    #theme(plot.margin = unit(c(1,1,5,1), "cm")) + 
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))  
+  
+  plot(bac.plot)
+  
+  if (make.pdf) {
+    dev.copy2pdf(file=paste0(base.name, "_bac_spikes.pdf"), width=16, height=10)
+    writeLines(paste0("Figure saved to: ", paste0(base.name, "_bac_spikes.pdf")))
+    d = dev.off()
+  }
 }
 attr(plot.bac.spikes, 'help') = "
 This function creates a bacterial spike plot.
@@ -194,16 +202,24 @@ plot.polya.spikes <- function(use.exprs, pgf.file, plot.type=c("AFFX-r2-Bs", "AF
   #from the affy exon/gene array whitepaper: lys<phe<thr<dap, not sure about Trpn
   spike.dta.merge$Spike <- factor(spike.dta.merge$Spike, levels=c("Dap", "Thr", "Phe", "Lys", "Trpn"), ordered=T)
   
-  pa.basic.plot <- qplot(x=Var2, y=value, group=Spike, color=Spike, data=spike.dta.merge[spike.dta.merge$type == plot.type,], stat="summary",
-                         fun.y=mean, geom="line", ylab="log2(Expression)", xlab="", main="PolyA Spikes", facets=pos~direction) + theme(axis.text.x=element_text(size=8, angle=90, hjust=1))
+  #pa.basic.plot <- qplot(x=Var2, y=value, group=Spike, color=Spike, data=spike.dta.merge[spike.dta.merge$type == plot.type,], stat="summary",
+  #                       fun.y=mean, geom="line", ylab="log2(Expression)", xlab="", main="PolyA Spikes", facets=pos~direction) + theme(axis.text.x=element_text(size=8, angle=90, hjust=1))
   
+  pa.basic.plot <- ggplot(spike.dta.merge[spike.dta.merge$type == plot.type,], aes(x=Var2, y=value, group=Spike, color=Spike)) +
+    stat_summary(fun.y=mean, geom="line") +
+    ylab("log2(Expression)") +
+    xlab("") +
+    ggtitle("PolyA Spikes") + 
+    #theme(plot.margin = unit(c(1,1,5,1), "cm")) + 
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    facet_grid(pos~direction)
   
   plot(pa.basic.plot)
-	
+  
   if (make.pdf) {
-	dev.copy2pdf(file=paste0(base.name, "_polya_spikes.pdf"), width=16, height=10)
-	writeLines(paste0("Figure saved to: ", paste0(base.name, "_polya_spikes.pdf")))
-	d = dev.off()
+    dev.copy2pdf(file=paste0(base.name, "_polya_spikes.pdf"), width=16, height=10)
+    writeLines(paste0("Figure saved to: ", paste0(base.name, "_polya_spikes.pdf")))
+    d = dev.off()
   }
 }
 attr(plot.polya.spikes, 'help') = "
@@ -227,17 +243,17 @@ base.name: The base filename of the PDF to create (default is 'test').
 #   An ExpressionSet
 heatmap.process <- function(use.exprs, num.genes)
 {
-    norm.exprs.basic <- use.exprs
-    norm.exprs.basic.mat <- exprs(norm.exprs.basic)
-    if(missing(num.genes) || is.null(num.genes) || is.na(num.genes))
-    {
-        return(norm.exprs.basic.mat)
-    } else {
-		exprs.var <- apply(norm.exprs.basic.mat, 1, var)
-        norm.exprs.basic.var <- norm.exprs.basic.mat[order(exprs.var, decreasing=TRUE),][1:num.genes,]
-        return(norm.exprs.basic.var)
-    }
-   
+  norm.exprs.basic <- use.exprs
+  norm.exprs.basic.mat <- exprs(norm.exprs.basic)
+  if(missing(num.genes) || is.null(num.genes) || is.na(num.genes))
+  {
+    return(norm.exprs.basic.mat)
+  } else {
+    exprs.var <- apply(norm.exprs.basic.mat, 1, var)
+    norm.exprs.basic.var <- norm.exprs.basic.mat[order(exprs.var, decreasing=TRUE),][1:num.genes,]
+    return(norm.exprs.basic.var)
+  }
+  
 }
 attr(heatmap.process, 'help') = "
 This function processes expression data for heatmaps.
@@ -245,7 +261,7 @@ This function processes expression data for heatmaps.
 Parameters:
 use.exprs: An ExpressionSet returned by the rma() function.
 num.genes: A number indicating the number of most variable genes to include. If NULL, all genes
-   will be included (default is 1000).
+will be included (default is 1000).
 
 Returns:
 A matrix of expression values.
@@ -270,17 +286,17 @@ make.heatmap <- function(use.exprs, cut.dist=NULL, num.genes=1000, base.factors=
   ## Add option of plotting RIN as continuous variable
   rin.discrete=T
   if (rin.discrete) {
-	if (!is.null(rin.breaks) && 'RIN' %in% names(all.dta)) {
-		all.dta$RIN = as.numeric(as.character(all.dta$RIN))
-		all.dta$RIN <- cut(all.dta$RIN, rin.breaks)
-		use.dta <- all.dta[,append(base.factors, "RIN", after=0)]
-	} else {
-		warning("RIN values were not found in the sample annotations.")
-		use.dta <- all.dta[,base.factors]
-	}
+    if (!is.null(rin.breaks) && 'RIN' %in% names(all.dta)) {
+      all.dta$RIN = as.numeric(as.character(all.dta$RIN))
+      all.dta$RIN <- cut(all.dta$RIN, rin.breaks)
+      use.dta <- all.dta[,append(base.factors, "RIN", after=0)]
+    } else {
+      warning("RIN values were not found in the sample annotations.")
+      use.dta <- all.dta[,base.factors]
+    }
   } else {
     all.dta$RIN = as.numeric(as.character(all.dta$RIN))
-	use.dta <- all.dta[,append(base.factors, "RIN", after=0)]
+    use.dta <- all.dta[,append(base.factors, "RIN", after=0)]
   }
   
   new.heat <- annHeatmap(norm.exprs.basic.var, annotation=use.dta, dendrogram = list(clustfun = hclust, distfun = dist, Col = list(status = "yes"), Row = list(status = "hidden")),
@@ -294,9 +310,9 @@ make.heatmap <- function(use.exprs, cut.dist=NULL, num.genes=1000, base.factors=
   plot(new.heat)
   
   if (make.pdf) {
-      d = dev.copy2pdf(file=paste0(base.name, '_heatmap.pdf'), width=16, height=10)
-      writeLines(paste0("Figure saved to: ", paste0(base.name, '_heatmap.pdf')))
-	  d = dev.off()
+    d = dev.copy2pdf(file=paste0(base.name, '_heatmap.pdf'), width=16, height=10)
+    writeLines(paste0("Figure saved to: ", paste0(base.name, '_heatmap.pdf')))
+    d = dev.off()
   }
 }
 attr(make.heatmap, 'help') = "
@@ -306,11 +322,11 @@ Parameters:
 use.exprs: An ExpressionSet returned by the rma() function.
 cut.dist: The height at which to cut the dendrogram (default is NULL; no cutting).
 num.genes: A number indicating the number of most variable genes to include. If NULL, all genes
-   will be included (default is 1000).
+will be included (default is 1000).
 base.factors: A character vector containing column names that will be used to annotate the
-   heatmap (default is c('Sex')).
+heatmap (default is c('Sex')).
 rin.breaks: A numeric vector indicating the break points for discretizing the RIN 
-   scores (default is c(0, 5, 7, 10)).
+scores (default is c(0, 5, 7, 10)).
 make.pdf: A logical indicating if a PDF should be created. 
 base.name: The base filename of the PDF to create (default is 'test').
 "
